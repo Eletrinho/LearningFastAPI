4ce1c8f5a907b05b1eca9ba6edd1df6cb313f235
Cauã <101751692+Eletrinho@users.noreply.github.com>
2024-01-04 15:00:11 -0300

    schemas e create_user feito e testado

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
 
+from fast_zero.schemas import UserDB, UserPublic, UserSchema
+
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo!'}
+
+
+database = []
+
+
+@app.post('/users/', status_code=201, response_model=UserPublic)
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
