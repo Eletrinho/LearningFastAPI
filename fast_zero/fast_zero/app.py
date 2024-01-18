d5384514eb6b438277b665e9d8d66fe9f592945f
Cauã <101751692+Eletrinho@users.noreply.github.com>
2024-01-18 12:33:27 -0300

    Implementação de ToDo

--- fast_zero/fast_zero/app.py
+++ fast_zero/fast_zero/app.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
 
-from routes import auth, users
+from routes import auth, todos, users
 
 app = FastAPI()
 
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(todos.router)
 
 
 @app.get('/')
